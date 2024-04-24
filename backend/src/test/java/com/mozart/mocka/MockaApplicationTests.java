0be9a54b1038f9972989be173a62ee5f82a93aab
김보경 <love2491193@naver.com>
2024-04-24 17:03:35 +0900

    🎉 begin: backend project start and ignore file

---
+++ backend/src/test/java/com/mozart/mocka/MockaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozart.mocka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
