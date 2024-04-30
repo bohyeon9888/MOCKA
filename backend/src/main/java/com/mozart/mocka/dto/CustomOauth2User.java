72c57c02cf846be7b0420253f0546234f7453591
bohyeon9888 <bohyeon9888@gmail.com>
2024-04-30 10:31:57 +0900

    ✨ feat: Oauth2 User Dto 추가

--- backend/src/main/java/com/mozart/mocka/dto/CustomOauth2User.java
+++ backend/src/main/java/com/mozart/mocka/dto/CustomOauth2User.java
@@ -1,4 +1,44 @@
 package com.mozart.mocka.dto;
 
-public class Oauth2User {
+import com.mozart.mocka.dto.response.Oauth2ResponseDto;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOauth2User implements OAuth2User {
+
+    private final Oauth2ResponseDto oAuth2Response;
+
+    public CustomOauth2User(Oauth2ResponseDto oAuth2Response) {
+        this.oAuth2Response = oAuth2Response;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return oAuth2Response.getName();
+    }
+
+    public String getMemberNickname() {
+        return oAuth2Response.getName();
+    }
+
+    public String getEmail() {
+        return oAuth2Response.getEmail();
+    }
+
+    public String getMemberProfile() {
+        return oAuth2Response.getProfileImg();
+    }
 }
