29927ff1efe1650eeba7ac400180ffcb255b9624
황정민 <sini3181@naver.com>
2024-05-01 17:03:05 +0900

    ✨ feat: jwt token에서 key값에 해당하는 value를 가져오는 함수 구현
    access token에서 프로필 사진과 닉네임 가져오기 위해 구현

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -16,3 +16,21 @@ export function logout() {
 export function getToken() {
   return Cookies.get("access_token");
 }
+
+export function getValueFromToken(key) {
+  const token = getToken().split(" ")[1];
+  if (!token) return undefined;
+
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join(""),
+  );
+
+  return JSON.parse(jsonPayload)[key];
+}
