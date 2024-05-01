e1e3c670a940bfe728696367c1c91f13612faf53
kang-dan <eodms4334@naver.com>
2024-05-01 16:11:04 +0900

    ✨ feat: 상단바 뷰어/편집모드 토글기능, 사이드바 프로필 추가, 메뉴 리스트 추가
    상단바랑 사이드바 조금씩 수정하고 있어용

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,40 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
 function header() {
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const toggleMode = () => {
+    setIsEditMode(!isEditMode);
+  };
+
   return (
     <header className="bg-gray-700">
-      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-2">
-        <a href="홈화면으로 이동할꺼야" className="flex items-center space-x-2">
-          <img src="/logo.svg" className="h-5" alt="Mocka Logo" />
-          <span className="text-h3 font-bold text-white">Mocka</span>
-        </a>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+      <div className="mx-auto flex  h-11 flex-wrap items-center justify-between p-2">
+        <div className="ml-11">
+          <a
+            href="홈화면으로 이동할꺼야"
+            className="flex items-center space-x-2"
+          >
+            <img src="/logo.svg" className="h-4" alt="Mocka Logo" />
+            <span className="text-h3 font-bold text-white">Mocka</span>
+          </a>
+        </div>
+        <div
+          className="mr-3 hidden w-full md:block md:w-auto"
+          id="navbar-default"
+        >
           <ul className="flex md:space-x-8 ">
             <li>
               <img
                 src="/public/asset/header/header-invite.svg"
-                className="h-5"
+                className="h-4 cursor-pointer"
                 alt="header-invite"
               />
             </li>
             <li>
               <img
                 src="/public/asset/header/header-link.svg"
-                className="h-5"
+                className="h-4 cursor-pointer"
                 alt="header-link"
               />
             </li>
-            <li>
-              <img
-                src="/public/asset/header/header-edit-mode.svg"
-                className="h-5"
-                alt="header-edit-mode"
-              />
+            <li onClick={toggleMode} className="cursor-pointer">
+              {isEditMode ? (
+                <img
+                  src="/public/asset/header/header-edit-mode.svg"
+                  className="h-5 cursor-pointer"
+                  alt="header-edit-mode"
+                />
+              ) : (
+                <img
+                  src="/public/asset/header/header-viewer-mode.svg"
+                  className="h-5 cursor-pointer"
+                  alt="header-edit-mode"
+                />
+              )}
             </li>
             <li>
               <img
                 src="/public/asset/header/header-option.svg"
-                className="h-5"
+                className="h-4 cursor-pointer"
                 alt="header-option"
               />
             </li>
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,29 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Sidebar() {
+  const [isRecentsOpen, setIsRecentsOpen] = useState(false);
+  const [isTeamsOpen, setIsTeamsOpen] = useState(false);
+
+  const toggleRecents = () => {
+    setIsRecentsOpen(!isRecentsOpen);
+  };
+
+  const toggleTeams = () => {
+    setIsTeamsOpen(!isTeamsOpen);
+  };
+
+  /*
+              <li onClick={toggleMode} className="cursor-pointer">
+              {isEditMode ? (
+                <img
+                  src="/public/asset/header/header-edit-mode.svg"
+                  className="h-5 cursor-pointer"
+                  alt="header-edit-mode"
+                />
+              ) : (
+                <img
+                  src="/public/asset/header/header-viewer-mode.svg"
+                  className="h-5 cursor-pointer"
+                  alt="header-edit-mode"
+                />
+              )}
+            </li>
+  */
+
   return (
-    <aside class="sidebar bottom-0 top-0 h-full w-[216px] overflow-y-auto bg-gray-100 p-2 text-center lg:left-0">
-      <div class="text-xl text-gray-700">
-        <div class="mt-1 flex justify-center p-5">
-          <h5 class="text-center font-bold text-gray-700">PROJECT LIST</h5>
+    <aside className="sidebar relative bottom-0 top-0 h-full w-[216px] overflow-y-auto bg-gray-100 p-2 text-center lg:left-0">
+      <div className="text-xl text-gray-700">
+        <div className="mt-1 flex justify-center p-5">
+          <h5 className="text-center font-bold text-gray-700">PROJECT LIST</h5>
         </div>
-        <div class="w-[170px]items-center my-2 h-[1px] bg-sidebar-division-color" />
+        <div className="mx-auto my-1 h-[1px] w-[170px] items-center border border-dashed bg-sidebar-division-color" />
       </div>
-      <div class="mt-3 flex cursor-pointer items-center rounded-md p-2.5 px-4 text-white duration-300 hover:bg-gray-300">
-        <img
-          src="/public/asset/sidebar/sidebar-right-pointer.svg"
-          className="h-3"
-          alt="sidebar-right-pointer"
-        />
-        <span class="text-h5 ml-7 font-bold text-gray-700">RECENTS</span>
+      <div className="mt-5">
+        <div
+          className="mt-4 flex cursor-pointer items-center rounded-md p-2.5 px-4 text-white duration-300 hover:bg-gray-300"
+          onClick={toggleRecents}
+        >
+          <div className="mr-7 w-3">
+            {isRecentsOpen ? (
+              <img
+                src="/public/asset/sidebar/sidebar-down-pointer.svg"
+                className="h-1.5 w-full"
+                alt="sidebar-down-pointer"
+              />
+            ) : (
+              <img
+                src="/public/asset/sidebar/sidebar-right-pointer.svg"
+                className="h-2.5 w-full"
+                alt="sidebar-right-pointer"
+              />
+            )}
+          </div>
+          <h5 className="font-bold text-gray-700">RECENTS</h5>
+        </div>
+        {isRecentsOpen && (
+          <div className="pl-12">
+            <div className="mt-2 flex cursor-pointer items-center rounded-md p-2.5 px-4 text-white duration-300 hover:bg-gray-300">
+              <h5 className="ml-3 font-bold text-gray-700">Recent Item 1</h5>
+            </div>
+            <div className="mt-2 flex cursor-pointer items-center rounded-md p-2.5 px-4 text-white duration-300 hover:bg-gray-300">
+              <h5 className="ml-3 font-bold text-gray-700">Recent Item 2</h5>
+            </div>
+          </div>
+        )}
+        <div
+          className="mt-4 flex cursor-pointer items-center rounded-md p-2.5 px-4 text-white duration-300 hover:bg-gray-300"
+          onClick={toggleTeams}
+        >
+          <div className="mr-7 w-3">
+            {isTeamsOpen ? (
+              <img
+                src="/public/asset/sidebar/sidebar-down-pointer.svg"
+                className="h-1.5 w-full"
+                alt="sidebar-down-pointer"
+              />
+            ) : (
+              <img
+                src="/public/asset/sidebar/sidebar-right-pointer.svg"
+                className="h-2.5 w-full"
+                alt="sidebar-right-pointer"
+              />
+            )}
+          </div>
+          <h5 className="font-bold text-gray-700">TEAMS</h5>
+        </div>
+        {isTeamsOpen && (
+          <div className="pl-12">
+            <div className="mt-2 flex cursor-pointer items-center rounded-md p-2.5 px-4 text-white duration-300 hover:bg-gray-300">
+              <h5 className="ml-3 font-bold text-gray-700">Team Item 1</h5>
+            </div>
+            <div className="mt-2 flex cursor-pointer items-center rounded-md p-2.5 px-4 text-white duration-300 hover:bg-gray-300">
+              <h5 className="ml-3 font-bold text-gray-700">Team Item 2</h5>
+            </div>
+          </div>
+        )}
       </div>
-      <div class="mt-3 flex cursor-pointer items-center rounded-md p-2.5 px-4 text-white duration-300 hover:bg-gray-300">
-        <img
-          src="/public/asset/sidebar/sidebar-right-pointer.svg"
-          className="h-3"
-          alt="sidebar-right-pointer"
-        />
-        <span class="text-h5 ml-7 font-bold text-gray-700">TEAMS</span>
+      <div className="absolute bottom-0 left-0 w-full text-xl text-gray-700">
+        <div className="mx-auto my-1 h-[1px] w-[170px] items-center bg-sidebar-division-color" />
+        <div className="mt-1 flex justify-center p-5">
+          <img
+            src="/public/asset/sidebar/sidebar-profile.svg"
+            className="absolute left-6 h-10 cursor-pointer"
+            alt="sidebar-profile"
+          />
+          <h5 className="my-1 ml-2 text-center font-bold text-gray-700">
+            Login
+          </h5>
+        </div>
       </div>
     </aside>
   );
