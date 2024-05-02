ed1fdc2b7a3a61ec152c3844996a32097c76b704
tjwjdgus83 <tjwjdgus83@naver.com>
2024-05-02 17:54:33 +0900

    ✨ feat: json 중첩구조 재귀 파싱 구현중

--- backend/src/main/java/com/mozart/mocka/service/generator/GenResponse.java
+++ backend/src/main/java/com/mozart/mocka/service/generator/GenResponse.java
@@ -1,31 +1,100 @@
 package com.mozart.mocka.service.generator;
 
-import com.mozart.mocka.dto.request.InitializerRequestDto;
-import com.mozart.mocka.repository.ApiPathRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mozart.mocka.domain.ApiResponse;
+import com.mozart.mocka.repository.ApiResponseRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class GenResponse {
-    private final ApiPathRepository apiPathRepository;
+    private final ApiResponseRepository apiResponseRepository;
     private static final String whiteSpace = "    ";
-    public static String makeStrObject(String dtoClassName, InitializerRequestDto request){
-        String packageName="package " + request.getSpringPackageName() + dtoClassName+";\n\n";
-        return "";
+
+    public void makeStrObject(String dtoClassName, String springPackageName, Long apiId) throws Exception{
+        String packageName="package " + springPackageName + dtoClassName+";\n\n";
+        List<ApiResponse> apiResponses=apiResponseRepository.findByApiProject_ApiId(apiId);
+        String classString=addAnnotation("public class "+dtoClassName+"{"+"\n");
+        if(apiResponses.isEmpty()) {
+            log.info("null");
+        }
+        else {
+            Set<String> appendedClassList = new HashSet<String>();
+            for (ApiResponse apiResponse : apiResponses) {
+                if(apiResponse.getType().equals("Object")){
+                    String type=apiResponse.getKey().substring(0, 1).toUpperCase() + apiResponse.getKey().substring(1);
+                    if (appendedClassList.contains(type)){
+                        type=type+"_";
+                        appendedClassList.add(type);
+                    }else{
+                        appendedClassList.add(type);
+                    }
+                    String fieldName=apiResponse.getKey().substring(0, 1).toLowerCase() + apiResponse.getKey().substring(1);
+                    String instanceVariable;
+
+                    if (apiResponse.getArrayList()){
+                        instanceVariable="private "+"List<"+type+">"+" "+fieldName+";"+"\n";
+                    }else{
+                        instanceVariable="private "+type+" "+fieldName+";"+"\n";
+                    }
+                    List<Map<String, Object>> mapList =
+                            new ObjectMapper().readValue(apiResponse.getData(), new TypeReference<List<Map<String, Object>>>(){});
+                    classString=classString+getDeepWhiteSpace(instanceVariable+recurInnerClass(mapList,type,appendedClassList,1));
+
+                }else{
+                    String filedName=apiResponse.getKey().substring(0, 1).toLowerCase() + apiResponse.getKey().substring(1);
+                    String instanceVariable=getDeepWhiteSpace("private "+apiResponse.getType()+" "+filedName+";"+"\n",1);
+                    classString=classString+instanceVariable;
+                }
+            }
+        }
+        classString=classString+"}"+"\n";
+        System.out.println("=========================");
+        System.out.println(classString);
+    }
+    public String recurInnerClass(List<Map<String, Object>> dataList,String className,Set<String> appendedClassList,int deep){
+        String classString=addAnnotation("public class "+className+"{"+"\n");
+        System.out.println("============================= "+className);
+        System.out.println(dataList.size());
+        for (Map<String, Object> data:dataList){
+            if(data.get("type").equals("Object")){
+                String keyValue= (String) data.get("key");
+                String type=keyValue.substring(0, 1).toUpperCase() + keyValue.substring(1);
+                if (appendedClassList.contains(type)){
+                    type=type+"_";
+                    appendedClassList.add(type);
+                }else{
+                    appendedClassList.add(type);
+                }
+                String fieldName=keyValue.substring(0, 1).toLowerCase() + keyValue.substring(1);
+                String instanceVariable;
+                if ((boolean) data.get("arrayList")){
+                    instanceVariable="private "+"List<"+type+">"+" "+fieldName+";"+"\n";
+                }else{
+                    instanceVariable="private "+type+" "+fieldName+";"+"\n";
+                }
+                List<Map<String, Object>> mapList=(List<Map<String,Object>>)data.get("value");
+                classString=classString+getDeepWhiteSpace(instanceVariable,1)+recurInnerClass(paramMap,type,appendedClassList,2);
+            }else{
+
+            }
+        }
+        System.out.println("=============================");
+        classString=classString+"}"+"\n";
+        return getDeepWhiteSpace(classString,deep);
     }
     public static String addAnnotation(String content){
-        return """
-            @NoArgsConstructor
-            @AllArgsConstructor
-            @Data
-            @Builder
-        """+"\n"+content;
+        return "@NoArgsConstructor"+"\n"+
+                "@AllArgsConstructor"+"\n"+
+                "@Data"+"\n"+
+                "@Builder"+"\n"+
+                content;
     }
 
     public static String getDeepWhiteSpace(String content,int deep){
