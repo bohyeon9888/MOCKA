43ad515c68faac64f4bb0f8d8f9e5fefe0ee9c2d
tjwjdgus83 <tjwjdgus83@naver.com>
2024-05-02 14:22:10 +0900

    ✨ feat: ResponseDtoGenerator 유틸성 메서드 생성
    반복적인으로 사용하게 될 함수를 미리 선언

--- backend/src/main/java/com/mozart/mocka/service/generator/GenRequest.java
+++ backend/src/main/java/com/mozart/mocka/service/generator/GenRequest.java
@@ -1,8 +1,13 @@
 package com.mozart.mocka.service.generator;
 
+import com.mozart.mocka.repository.ApiPathRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class GenRequest {
-
+    private final ApiPathRepository apiPathRepository;
 }
--- backend/src/main/java/com/mozart/mocka/service/generator/GenResponse.java
+++ backend/src/main/java/com/mozart/mocka/service/generator/GenResponse.java
@@ -1,8 +1,42 @@
 package com.mozart.mocka.service.generator;
 
+import com.mozart.mocka.dto.request.InitializerRequestDto;
+import com.mozart.mocka.repository.ApiPathRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class GenResponse {
+    private final ApiPathRepository apiPathRepository;
+    private static final String whiteSpace = "    ";
+    public static String makeStrObject(String dtoClassName, InitializerRequestDto request){
+        String packageName="package " + request.getSpringPackageName() + dtoClassName+";\n\n";
+        return "";
+    }
+    public static String addAnnotation(String content){
+        return """
+            @NoArgsConstructor
+            @AllArgsConstructor
+            @Data
+            @Builder
+        """+"\n"+content;
+    }
 
+    public static String getDeepWhiteSpace(String content,int deep){
+        String[] lines=content.split(System.lineSeparator());
+        for(int i =0;i<lines.length;i++){
+            lines[i]=whiteSpace.repeat(deep)+lines[i];
+        }
+        StringBuilder resultLines = new StringBuilder();
+        for(String line : lines){
+            resultLines.append(line);
+        }
+        return resultLines.toString();
+    }
 }
