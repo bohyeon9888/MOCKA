de9d4d3250cea51b583fa4bdd8a620b86525d9c3
tjwjdgus83 <tjwjdgus83@naver.com>
2024-05-03 10:42:41 +0900

    ✨ feat: responseDto 문자열 추출

--- backend/src/main/java/com/mozart/mocka/service/generator/GenResponse.java
+++ backend/src/main/java/com/mozart/mocka/service/generator/GenResponse.java
@@ -17,80 +17,111 @@ public class GenResponse {
     private final ApiResponseRepository apiResponseRepository;
     private static final String whiteSpace = "    ";
 
-    public void makeStrObject(String dtoClassName, String springPackageName, Long apiId) throws Exception{
-        String packageName="package " + springPackageName + dtoClassName+";\n\n";
+    public String makeStrObject(String dtoClassName, String springPackageName, Long apiId) throws Exception{
+        String packageName="package " + springPackageName +";\n";
+        packageName=addPackage(packageName);
         List<ApiResponse> apiResponses=apiResponseRepository.findByApiProject_ApiId(apiId);
-        String classString=addAnnotation("public class "+dtoClassName+"{"+"\n");
+        String classOuterString=addAnnotation("public class "+dtoClassName+"{"+"\n");
+        String classInnerString="";
         if(apiResponses.isEmpty()) {
             log.info("null");
         }
         else {
-            Set<String> appendedClassList = new HashSet<String>();
+            Set<String> appendedClassNameList = new HashSet<String>();
+            Set<String> fieldNameList = new HashSet<String>();
             for (ApiResponse apiResponse : apiResponses) {
+                //필드네임 정하기
+                String fieldName=apiResponse.getKey().substring(0, 1).toLowerCase() + apiResponse.getKey().substring(1);
+                if( fieldNameList.contains(fieldName)){
+                    fieldName=fieldName+"Copy";
+                    fieldNameList.add(fieldName);
+                }else{
+                    fieldNameList.add(fieldName);
+                }
+                
                 if(apiResponse.getType().equals("Object")){
-                    String type=apiResponse.getKey().substring(0, 1).toUpperCase() + apiResponse.getKey().substring(1);
-                    if (appendedClassList.contains(type)){
-                        type=type+"_";
-                        appendedClassList.add(type);
+                    //클래스 이름 정하기
+                    String className=apiResponse.getKey().substring(0, 1).toUpperCase() + apiResponse.getKey().substring(1);
+                    if (appendedClassNameList.contains(className)){
+                        className=className+"Copy";
+                        appendedClassNameList.add(className);
                     }else{
-                        appendedClassList.add(type);
+                        appendedClassNameList.add(className);
                     }
-                    String fieldName=apiResponse.getKey().substring(0, 1).toLowerCase() + apiResponse.getKey().substring(1);
-                    String instanceVariable;
-
-                    if (apiResponse.getArrayList()){
-                        instanceVariable="private "+"List<"+type+">"+" "+fieldName+";"+"\n";
-                    }else{
-                        instanceVariable="private "+type+" "+fieldName+";"+"\n";
+                    String type=className;
+                    if(apiResponse.getArrayList()){
+                        type="List<"+type+">";
                     }
-                    List<Map<String, Object>> mapList =
+                    String instanceVariable=type+" "+fieldName+";"+"\n";
+                    //data안의 json을 로드 후 map으로 변환
+                    List<Map<String, Object>> dataMap =
                             new ObjectMapper().readValue(apiResponse.getData(), new TypeReference<List<Map<String, Object>>>(){});
-                    classString=classString+getDeepWhiteSpace(instanceVariable+recurInnerClass(mapList,type,appendedClassList,1));
-
+                    classInnerString=classInnerString+instanceVariable+makeInnerClass(dataMap,className,appendedClassNameList);
                 }else{
-                    String filedName=apiResponse.getKey().substring(0, 1).toLowerCase() + apiResponse.getKey().substring(1);
-                    String instanceVariable=getDeepWhiteSpace("private "+apiResponse.getType()+" "+filedName+";"+"\n",1);
-                    classString=classString+instanceVariable;
+                    String type=apiResponse.getType();
+                    if(apiResponse.getArrayList()){
+                        type="List<"+type+">";
+                    }
+                    String instanceVariable=type+" "+fieldName+";"+"\n";
+                    classInnerString=classInnerString+instanceVariable;
                 }
             }
         }
-        classString=classString+"}"+"\n";
-        System.out.println("=========================");
-        System.out.println(classString);
+        return packageName+classOuterString+getDeepWhiteSpace(classInnerString,1)+"}"+"\n";
     }
-    public String recurInnerClass(List<Map<String, Object>> dataList,String className,Set<String> appendedClassList,int deep){
-        String classString=addAnnotation("public class "+className+"{"+"\n");
-        System.out.println("============================= "+className);
-        System.out.println(dataList.size());
+    public String makeInnerClass(List<Map<String, Object>> dataList,String className,Set<String> appendedClassNameList){
+        String classOuterString=addAnnotation("public static class "+className+"{"+"\n");
+        String classInnerString="";
+        Set<String> fieldNameList = new HashSet<String>();
         for (Map<String, Object> data:dataList){
-            if(data.get("type").equals("Object")){
-                String keyValue= (String) data.get("key");
-                String type=keyValue.substring(0, 1).toUpperCase() + keyValue.substring(1);
-                if (appendedClassList.contains(type)){
-                    type=type+"_";
-                    appendedClassList.add(type);
-                }else{
-                    appendedClassList.add(type);
-                }
-                String fieldName=keyValue.substring(0, 1).toLowerCase() + keyValue.substring(1);
-                String instanceVariable;
-                if ((boolean) data.get("arrayList")){
-                    instanceVariable="private "+"List<"+type+">"+" "+fieldName+";"+"\n";
-                }else{
-                    instanceVariable="private "+type+" "+fieldName+";"+"\n";
-                }
-                List<Map<String, Object>> mapList=(List<Map<String,Object>>)data.get("value");
-                classString=classString+getDeepWhiteSpace(instanceVariable,1)+recurInnerClass(paramMap,type,appendedClassList,2);
+            String keyValue= (String) data.get("key");
+            String fieldName=keyValue.substring(0, 1).toLowerCase() + keyValue.substring(1);
+            if( fieldNameList.contains(fieldName)){
+                fieldName=fieldName+"Copy";
+                fieldNameList.add(fieldName);
             }else{
+                fieldNameList.add(fieldName);
+            }
+            String type=data.get("type").toString();
+            boolean isArray=(boolean) data.get("arrayList");
+            if(type.equals("Object")){
+                //내부 클래스 이름 정하기
+                String innerClassName=keyValue.substring(0, 1).toUpperCase() + keyValue.substring(1);
+                if (appendedClassNameList.contains(innerClassName)){
+                    innerClassName=innerClassName+"Copy";
+                    appendedClassNameList.add(innerClassName);
+                }else{
+                    appendedClassNameList.add(innerClassName);
+                }
+                type=innerClassName;
+                if((boolean) data.get("arrayList")){
+                    type="List<"+type+">";
+                }
+                String instanceVariable=type+" "+fieldName+";"+"\n";
+                List<Map<String, Object>> valueMap=(List<Map<String,Object>>)data.get("value");
 
+                classInnerString=classInnerString+instanceVariable+makeInnerClass(valueMap,innerClassName,appendedClassNameList);
+            }else{
+                if(isArray){
+                    type="List<"+type+">";
+                }
+                String instanceVariable=type+" "+fieldName+";"+"\n";
+                classInnerString=classInnerString+instanceVariable;
             }
         }
-        System.out.println("=============================");
-        classString=classString+"}"+"\n";
-        return getDeepWhiteSpace(classString,deep);
+        return classOuterString+getDeepWhiteSpace(classInnerString,1)+"}"+"\n";
+    }
+
+
+    public static String addPackage(String content){
+        return  content+"import lombok.AllArgsConstructor;"+"\n"+
+                "import lombok.Builder;"+"\n"+
+                "import lombok.Data;"+"\n"+
+                "import lombok.NoArgsConstructor;"+"\n"+
+                "import java.util.List;"+"\n";
     }
     public static String addAnnotation(String content){
-        return "@NoArgsConstructor"+"\n"+
+        return  "@NoArgsConstructor"+"\n"+
                 "@AllArgsConstructor"+"\n"+
                 "@Data"+"\n"+
                 "@Builder"+"\n"+
@@ -98,13 +129,17 @@ public class GenResponse {
     }
 
     public static String getDeepWhiteSpace(String content,int deep){
-        String[] lines=content.split(System.lineSeparator());
+        String[] lines=content.split("\n");
+        System.out.println("============================go");
+        System.out.println(Arrays.deepToString(lines)+lines.length);
+        System.out.println("============================end");
         for(int i =0;i<lines.length;i++){
             lines[i]=whiteSpace.repeat(deep)+lines[i];
         }
         StringBuilder resultLines = new StringBuilder();
         for(String line : lines){
             resultLines.append(line);
+            resultLines.append("\n");
         }
         return resultLines.toString();
     }
