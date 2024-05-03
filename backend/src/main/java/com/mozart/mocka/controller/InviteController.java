b35fb9c7ec736bbc66c913c3ea74f589d50e925a
bohyeon9888 <bohyeon9888@gmail.com>
2024-05-03 09:09:48 +0900

    🔧 config: application-env 경로 수정

--- backend/src/main/java/com/mozart/mocka/controller/InviteController.java
+++ backend/src/main/java/com/mozart/mocka/controller/InviteController.java
@@ -4,6 +4,8 @@ import com.mozart.mocka.dto.request.InviteDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -11,6 +13,12 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/invite")
 public class InviteController {
+
+    @GetMapping("/test")
+    public void testApi() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        log.info("auth name : " + auth.getName());
+    }
 
     @PostMapping
     public ResponseEntity<?> inviteMember(@RequestBody InviteDto inviteDto) {
