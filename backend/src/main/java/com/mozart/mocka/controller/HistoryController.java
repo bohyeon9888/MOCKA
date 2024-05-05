9c61ed0a8d27ac004f39ea153a18506d683b790e
bohyeon9888 <bohyeon9888@gmail.com>
2024-05-06 02:26:17 +0900

    ✨ feat: member 권한 설정 request dto 및 controller 작성

---
+++ backend/src/main/java/com/mozart/mocka/controller/HistoryController.java
@@ -0,0 +1,29 @@
+package com.mozart.mocka.controller;
+
+import com.mozart.mocka.dto.request.ProjectAuthRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/history")
+public class HistoryController {
+
+    @PatchMapping("/authority")
+    public ResponseEntity<?> patchMemberAuthority(@RequestBody ProjectAuthRequestDto authRequestDto) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+
+}
