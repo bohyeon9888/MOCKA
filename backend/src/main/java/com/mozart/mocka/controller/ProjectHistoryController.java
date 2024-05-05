3263372ab21b284bbe4cdae4009d75ada24a7c28
bohyeon9888 <bohyeon9888@gmail.com>
2024-05-06 02:49:25 +0900

    💄 style: 파일 이름 변경 및 안 쓰는 라이브러리 정리

--- backend/src/main/java/com/mozart/mocka/controller/ProjectHistoryController.java
+++ backend/src/main/java/com/mozart/mocka/controller/ProjectHistoryController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/history")
-public class HistoryController {
+public class ProjectHistoryController {
 
     @PatchMapping("/authority")
     public ResponseEntity<?> patchMemberAuthority(@RequestBody ProjectAuthRequestDto authRequestDto) {
