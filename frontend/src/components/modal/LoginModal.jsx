6ab4cc4c043ffc84fdd047eecaa133b33f90df89
황정민 <sini3181@naver.com>
2024-05-07 09:37:24 +0900

    ✨ feat: Invite 모달 작성

--- frontend/src/components/modal/LoginModal.jsx
+++ frontend/src/components/modal/LoginModal.jsx
@@ -4,13 +4,14 @@ export default function LoginModal() {
   };
 
   return (
-    <div className="space-y-[30px] px-5">
+    <div className="space-y-[30px] px-10 pb-6">
+      <div className="w-full text-center text-2 font-extrabold">Sign in</div>
       <div
-        className="mt-[3px] flex h-[60px] w-[380px] cursor-pointer items-center justify-center space-x-[10px] rounded-full border-2 p-2"
+        className="mt-[3px] flex h-[60px] w-[380px] cursor-pointer items-center justify-center space-x-[10px] rounded-full border-2 p-2 text-gray-500 duration-200 hover:border-gray-500 hover:text-gray-600"
         onClick={googleLogin}
       >
         <img className="size-[30px]" src="/asset/google.svg" />
-        <div className="text-3 text-gray-500">Sign in using Google</div>
+        <div className="text-3 ">Sign in using Google</div>
       </div>
       <div className="text-center text-4 leading-normal text-gray-500">
         <div>nice to meet you!</div>
