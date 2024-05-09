9bbd14943983295dbe542f67ee7445361105fe6c
황정민 <sini3181@naver.com>
2024-05-10 00:34:20 +0900

    ✨ feat: Initializer 파일 다운로드 로직 작성

--- frontend/src/apis/initializer.js
+++ frontend/src/apis/initializer.js
@@ -1,9 +1,27 @@
 import axios from "./authAxios";
 import formatInitializerRequest from "../utils/formatInitializerRequest";
 
-const downloadSpringInitializer = async (initializerSetting) => {
+const downloadSpringInitializer = async ({
+  initializerSetting,
+  projectName,
+}) => {
   const formattedSetting = formatInitializerRequest(initializerSetting);
-  const { data } = await axios.post("initializer/create/24", formattedSetting);
+  const { data } = await axios({
+    method: "post",
+    url: `${import.meta.env.VITE_BASE_URL}initializer/create/1`,
+    data: formattedSetting,
+    responseType: "blob",
+  });
+
+  const url = window.URL.createObjectURL(new Blob([data]));
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", `${projectName || "projectName"}.zip`);
+  document.body.append(link);
+  link.click();
+
+  link.parentNode.removeChild(link);
+  window.URL.revokeObjectURL(url);
 
   return data;
 };
