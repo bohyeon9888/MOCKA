08b8d0cdbf437f8ff55d4d9df657a824c4785a68
황정민 <sini3181@naver.com>
2024-05-09 10:08:20 +0900

    🚑 Hotfix: accessToken 변수명 통일

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,7 +1,7 @@
 import Cookies from "js-cookie";
 
 export function isAuthenticated() {
-  const token = Cookies.get("access_token");
+  const token = Cookies.get("accessToken");
   return !!token;
 }
 
