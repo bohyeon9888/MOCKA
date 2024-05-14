b3e8cce0a1379e8d07f7558a72d899fa4e5129a2
tjwjdgus83 <tjwjdgus83@naver.com>
2024-05-14 14:21:54 +0900

    📝 doc: cors 설정 변경

--- mockserver/src/main/java/com/mozart/mockserver/config/webConfig.java
+++ mockserver/src/main/java/com/mozart/mockserver/config/webConfig.java
@@ -4,14 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class webConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
-                .allowedHeaders("headers")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
                 .maxAge(6000);
     }
 }
