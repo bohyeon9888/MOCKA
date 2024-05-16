e5fec084d2a707a8677ebbf15fc51595eb796a95
황정민 <sini3181@naver.com>
2024-05-16 17:52:26 +0900

    🐛 fix: query string 로직 충돌 해결

--- frontend/src/contexts/LanguageContext.jsx
+++ frontend/src/contexts/LanguageContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import updateQueryParam from "../utils/updateQueryParam";
 
 const LanguageContext = createContext();
 
@@ -9,7 +10,12 @@ export const LanguageProvider = ({ children }) => {
   const [language, setLanguage] = useState(initialLanguage);
 
   useEffect(() => {
-    setSearchParams({ lang: language });
+    updateQueryParam({
+      key: "lang",
+      value: language,
+      searchParams,
+      setSearchParams,
+    });
   }, [language, setSearchParams]);
 
   return (
